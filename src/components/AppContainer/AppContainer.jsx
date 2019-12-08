ed62ecbe74e1550c2df75bdde985098505a2b5c5
Jorge <jofer86@gmail.com>
2019-12-08 12:00:29 -0600

    Use styled-component to render the style of the individual components

--- src/components/AppContainer/AppContainer.jsx
+++ src/components/AppContainer/AppContainer.jsx
@@ -1,34 +1,90 @@
 import React from 'react';
+import styled from 'styled-components';
 import BookList from '../BooksList/BooksList';
 import BookForm from '../BooksForm/BooksForm';
 
-import './AppContainer.style.css';
-
-
 const AppContainer = () => {
-  return (
-    <div className="AppContainer">
+  const TopMenu = styled.div`
+  height: 95px;
+  width: 100%;
+  background-color: orangered;
+  header {
+    padding: 30px 100px;
+    background-color: gray;
+    position: relative;
+    h2 {
+      display: inline-block;
+      font-family: Montserrat;
+      font-size: 30px;
+      font-weight: bold;
+      font-stretch: normal;
+      font-style: normal;
+      line-height: normal;
+      letter-spacing: normal;
+      color: #0290ff;
+    }
+    a {
+      margin-left: 47px;
+      font-family: Montserrat;
+      font-size: 13px;
+      font-weight: normal;
+      font-stretch: normal;
+      font-style: normal;
+      line-height: normal;
+      letter-spacing: 1.9px;
+      color: #121212;
+    }
+    input {
+      margin-left: 41px;
+      opacity: 0.5;
+      font-family: Montserrat;
+      font-size: 13px;
+      font-weight: normal;
+      font-stretch: normal;
+      font-style: normal;
+      line-height: normal;
+      letter-spacing: 1.9px;
+    }
+    span {
+      position: absolute;
+      right: 100px;
+      bottom: 25%;
+      width: 45px;
+      height: 45px;
+      padding: 5px;
+      border: 1px solid black;
+      border-radius: 50%;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      background-color: whitesmoke;
+    }
+  }
+  `;
 
-      <div className="TopMenu">
+  const BookContainer = styled.div`
+    margin: 0 auto;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  `;
+  return (
+    <BookContainer>
+      <TopMenu>
         <header>
-          <h2 className="headerTitle">Bookstore CMS</h2>
+          <h2>Bookstore CMS</h2>
           <a href="#">Books</a>
-          <input type="text" placeholder="CATEGORIES"/>
-          <span className="circle">
+          <input type="text" placeholder="CATEGORIES" />
+          <span>
             <i class="fas fa-user"></i>
           </span>
-          
         </header>
-      </div>
-
-      
+      </TopMenu>
       <BookList />
-      
-
       <div className="FootMenu">
         <BookForm />
       </div>
-    </div>
+    </BookContainer>
   );
 };
 
--- src/components/Book/Book.jsx
+++ src/components/Book/Book.jsx
@@ -1,15 +1,106 @@
 import React from 'react';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import PropTypes from 'prop-types';
-import './Book.style.css';
+import styled from 'styled-components';
 
 
 const Book = (props) => {
+  const BookContainer = styled.div`
+    border-radius: 4px;
+    border: solid 1px #e8e8e8;
+    background-color: #ffffff;
+    display: flex;
+    align-items: center;
+    position: relative;
+    margin: 20px 0;
+    width: 1300px;
+  
+    .bookContainer-id{
+      font-weight: bold;
+      padding: 10px;
+    }
+  
+    .bookContainer-bookName{
+      padding: 10px;
+      span{
+        display: block;
+      }
+  
+      .bookContainer-bookName__utilites{
+        margin-top: 10px;
+        font-size: 14px;
+        display: flex;
+        font-weight: 300;
+        font-stretch: normal;
+        font-style: normal;
+        line-height: normal;
+        letter-spacing: normal;
+        color: #4386bf;
+        span{
+          cursor: pointer;
+          display: inline;
+          padding: 5px 10px;
+          border-right: 1px solid rgba(128, 128, 128, 0.295);
+          &:last-child{
+            border-right: none;
+          }
+          &:first-child{
+            padding-left: 0;
+          }
+          &:hover{
+            text-decoration: underline;
+          }
+        }
+      }
+    }
+  
+    .bookContainer-title{
+      font-size: 22px;
+      font-weight: bold;
+      font-stretch: normal;
+      font-style: normal;
+      line-height: normal;
+      letter-spacing: -0.2px;
+      color: #121212;
+    }
+  
+    .bookContainer-category{
+      opacity: 0.5;
+      font-family: Montserrat;
+      font-size: 14px;
+      font-weight: bold;
+      font-stretch: normal;
+      font-style: normal;
+      line-height: normal;
+      letter-spacing: normal;
+      color: #121212;
+    }
+  
+    .bookContainer-remove{
+      position: absolute;
+      right: 25px;
+      width: 84px;
+      height: 33px;
+      border: 1px solid #0290ff;
+      border-radius: 3px;
+      background-color: #0290ff;
+      font-size: 13px;
+      font-weight: 300;
+      font-stretch: normal;
+      font-style: normal;
+      line-height: normal;
+      letter-spacing: 0.5px;
+      text-align: center;
+      color: #ffffff;
+      margin-left: 100px;
+    }
+  }
+  `;
   const {
     id, title, category, removeBook,
   } = props;
   return (
-    <div className="bookContainer">
+    <BookContainer>
       <span className="bookContainer-id">{ id }</span>
       <div className="bookContainer-bookName">
         <span className="bookContainer-category">{ category }</span>
@@ -20,7 +111,7 @@ const Book = (props) => {
         </div>
       </div>
       <button className="bookContainer-remove" type="button" onClick={() => removeBook(id)}>Remove</button>
-    </div>
+    </BookContainer>
   );
 };
 
